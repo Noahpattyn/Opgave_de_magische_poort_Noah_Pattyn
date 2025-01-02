494c02e1400e9ea805995181b71e664630de7f16
Noah Pattyn <noah.pattyn@gmail.com>
2025-01-02 14:19:04 +0100

    Add project files.

---
+++ Opgave_de_magische_poort_Noah_Pattyn/Components/Data/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Opgave_de_magische_poort_Noah_Pattyn.Components.Data
+{
+    public class Models
+    {
+        public enum Kleur
+        {
+            Rood, Blauw, Geel, Groen, Zwart, Wit
+        }
+        public class Poort
+        {
+            public Kleur Kleur { get; set; }
+            public int Grootte { get; set; }
+            public int AantalInkeppingen { get; set; }
+            public int Gewicht { get; set; }
+
+        }
+    }
+}
